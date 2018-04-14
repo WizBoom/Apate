9ab5e8f4aade30d389e8311366a45395cf513704
Brian Van Hyfte <brian.van.hyfte@gmail.com>
2018-04-14 22:01:57 +0200

    * Please work now templates

--- auth/corp_management/app.py
+++ auth/corp_management/app.py
@@ -3,7 +3,7 @@ from flask_login import login_required
 from auth.util import Util
 from auth.decorators import needs_permission
 # Create and configure app
-Application = Blueprint('corp_management', __name__, template_folder='../templates/corp_management', static_folder='static')
+Application = Blueprint('corp_management', __name__, template_folder='templates/corp_management', static_folder='static')
 
 # Util
 Util = Util(
@@ -15,4 +15,4 @@ Util = Util(
 @login_required
 @needs_permission('admin', 'Corp management Landing')
 def index():
-    return render_template('index.html')
+    return render_template('corp_management/index.html')
