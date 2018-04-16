1cb30c2e90cbd347308abe5b14e7006208cfc289
Brian Van Hyfte <brian.van.hyfte@gmail.com>
2018-04-17 00:05:44 +0200

    * Added dynamic columns and corp information on landing page

--- auth/hr/app.py
+++ auth/hr/app.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from flask_login import login_required
+from auth.models import *
 
 # Create and configure app
 Application = Blueprint('hr', __name__, template_folder='templates/hr', static_folder='static')
 
 
-@Application.route('/', methods=['GET', 'POST'])
+@Application.route('/')
 @login_required
 def index():
     """Landing page of the hr module
@@ -16,4 +17,8 @@ def index():
     Returns:
         str: redirect to the appropriate url.
     """
-    return render_template('hr/index.html')
+
+    # Get all corporations that are open for recruitment
+    openCorporations = [corp for corp in Alliance.query.filter_by(id=current_app.config["ALLIANCE_ID"]).first().corporations if corp.recruitment_open]
+
+    return render_template('hr/index.html', open_corporations=openCorporations)
